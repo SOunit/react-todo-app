a6b8babded256113e9c88be3540b7861b6de1144
SOunit <workbookwork7@gmail.com>
2020-10-29 12:16:22 +0900

    add eidt to do function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import Header from './components/Header/Header';
 import EditToDo from './containers/EditToDo/EditToDo';
 
---
+++ src/components/Card/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Card.module.css';
+
+const card = (props) => {
+  return (
+    <div className={classes.Card} onClick={props.clicked}>
+      <h2>{props.title}</h2>
+      <p>{props.memo}</p>
+    </div>
+  );
+};
+
+export default card;
