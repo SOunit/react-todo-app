6e20fa744da1c42c7e92c5ed31452cff0cc61ecb
SOunit <workbookwork7@gmail.com>
2020-10-31 20:22:58 +0900

    add route and navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import classes from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 const header = (props) => {
   return (
-    <header>
-      <h1>To do {props.title}</h1>
+    <header class={classes.header}>
+      <nav>
+        <NavLink to="/" className={classes.Logo}>
+          <h1>To do {props.title}</h1>
+        </NavLink>
+        <NavLink to="/" exact activeClassName={classes.active}>
+          Todo
+        </NavLink>
+        <NavLink to="/done" activeClassName={classes.active}>
+          Done
+        </NavLink>
+        <NavLink to="/delete" activeClassName={classes.active}>
+          Delete
+        </NavLink>
+      </nav>
     </header>
   );
 };
---
+++ src/containers/Done/Done.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Done extends Component {
+  render = () => {
+    return (
+      <div>
+        <h2>Done page</h2>
+      </div>
+    );
+  };
+}
+
+export default Done;
